5be73d0837dff005beef2b065a2a42d35f3cd712
Adrian L Lange <git@p3lim.net>
2024-02-16 18:07:04 +0100

    Use keymap to deal with norwegian keys
    It's a bit more portable

--- common/custom.c
+++ common/custom.c
@@ -39,46 +39,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record){
                 unregister_code(state_esc ? KC_ESCAPE : KC_GRAVE);
             }
             return false;
-        case C_NO1: // æ
-            if(record->event.pressed){
-                // we use shift for A and E to capitalize, no need to handle this here
-                tap_code(KC_RCTL);
-                tap_code(KC_A);
-                tap_code(KC_E);
-            }
-            return false;
-        case C_NO2: // ø
-            if(record->event.pressed){
-                // the "/" symbol can't be shifted, so we have to deal with that
-                if(shifted){
-                    unregister_code(KC_LSFT); // reset shift state, I never use RSFT
-                    tap_code(KC_RCTL);
-                    tap_code(KC_SLSH);
-                    tap_code16(S(KC_O)); // shifted
-                    register_code(KC_LSFT); // restore shift state
-                } else {
-                    tap_code(KC_RCTL);
-                    tap_code(KC_SLSH);
-                    tap_code(KC_O);
-                }
-            }
-            return false;
-        case C_NO3: // å
-            if(record->event.pressed){
-                // the "o" character can't be shifted, so we have to deal with that
-                if(shifted){
-                    unregister_code(KC_LSFT); // reset shift state, I never use RSFT
-                    tap_code(KC_RCTL);
-                    tap_code(KC_O);
-                    tap_code16(S(KC_A)); // shifted
-                    register_code(KC_LSFT); // restore shift state
-                } else {
-                    tap_code(KC_RCTL);
-                    tap_code(KC_O);
-                    tap_code(KC_A);
-                }
-            }
-            return false;
         case C_SPAM:
             state_spam = record->event.pressed;
             state_spam_timer = timer_read();
--- common/custom.h
+++ common/custom.h
@@ -2,11 +2,13 @@
 
 #include QMK_KEYBOARD_H
 
+// requires altgr-intl keymap ("English (intl., with AltGr dead keys" in Gnome)
+#define C_NO1 RALT(KC_Z) // æ
+#define C_NO2 RALT(KC_L) // ø
+#define C_NO3 RALT(KC_W) // å
+
 enum {
     C_ESC0 = SAFE_RANGE, // layer 0 esc
     C_ESC1,              // layer 1 esc
-    C_NO1,               // æ, requires RCTL to be a compose key in software
-    C_NO2,               // ø, requires RCTL to be a compose key in software
-    C_NO3,               // å, requires RCTL to be a compose key in software
     C_SPAM
 };
